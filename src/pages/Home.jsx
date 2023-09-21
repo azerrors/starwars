4d05755956bd42f7a13c2fd8e2ba4317a7d7a425
stoidev <azer.nagi.2005@gmail.com>
2023-09-21 21:40:02 +0400

    asd

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,33 +13,33 @@ function Home() {
 
   return (
     <div className="relative h-[52rem]">
-      <div className=" absolute left-0 top-0 h-full w-full bg-[url('../../public/homebg.jpg')] bg-cover bg-center bg-no-repeat mix-blend-soft-light blur-sm dark:mix-blend-difference"></div>
+      <div className=" absolute left-0 top-0 homebg h-full w-full bg-[url('../../public/homebg.jpg')] bg-cover bg-center bg-no-repeat mix-blend-soft-light blur-sm dark:mix-blend-difference"></div>
       <h1 className="relative p-5 text-center text-xl  font-semibold uppercase tracking-widest text-cyan-500 dark:text-yellow-400 md:text-2xl">
         Hello {username ? username : "stranger"}, welcome to star wars fan page
       </h1>
       <div className="mt-10  flex flex-col items-center gap-6 md:mt-24 md:grid md:grid-cols-3 md:grid-rows-2 md:place-items-center">
         <Link to="/people" className={divStyle}>
-          <img src="../../public/character.jpg" className={imgStyle} alt="" />
+          <img src="../public/character.jpg " className={imgStyle} alt="" />
           <h3 className={h3Style}>Characters</h3>
         </Link>
         <Link to="/species" className={divStyle}>
-          <img src="../../public/species.jpg" className={imgStyle} alt="" />
+          <img src="../public/species.jpg" className={imgStyle} alt="" />
           <h3 className={h3Style}>Species</h3>
         </Link>
         <Link to="/starships" className={divStyle}>
-          <img src="../../public/starship.jpg" className={imgStyle} alt="" />
+          <img src="../public/starship.jpg" className={imgStyle} alt="" />
           <h3 className={h3Style}>Starsips</h3>
         </Link>
         <Link to="/vehicles" className={divStyle}>
-          <img src="../../public/vehicles.jpg" className={imgStyle} alt="" />
+          <img src="../public/vehicles.jpg" className={imgStyle} alt="" />
           <h3 className={h3Style}>Vehicles</h3>
         </Link>
         <Link to="/planet" className={divStyle}>
-          <img src="../../public/planet.png" className={imgStyle} alt="" />
+          <img src="../public/planet.png" className={imgStyle} alt="" />
           <h3 className={h3Style}>Planets</h3>
         </Link>
         <Link to="/films" className={divStyle}>
-          <img src="../../public/film.png" className={imgStyle} alt="" />
+          <img src="../public/film.png" className={imgStyle} alt="" />
           <h3 className={h3Style}>Films</h3>
         </Link>
       </div>
